d2813c552acbcb28a5a7e3154f3ebfe06bf18d5b
Takasur Azeem <takasurazeem@gmail.com>
2022-06-18 23:13:06 +0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Takasur Azeem on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
