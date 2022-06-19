57b085a6d0f710bfeb9470bc2b9b700138dfa6f0
Takasur Azeem <takasurazeem@gmail.com>
2022-06-19 21:16:40 +0500

    Responding to state changes using onChange()
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var blurAmount = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { newValue in
+                    print("New value is \(newValue)")
+                }
+        }
     }
 }
 
